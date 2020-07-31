97aa53bcfffe40db2973889ff8316a3464257e0a
Pravpro <praviradlakha@hotmail.com>
2020-07-31 18:44:45 -0400

    Added Combo button functionalities

--- assets/js/firstToThirdPerson.js
+++ assets/js/firstToThirdPerson.js
@@ -1,3 +1,25 @@
 $("#add-combo").click(function(event) {
 	$("#new-combo").toggleClass('d-none');
 });
+
+$("#combos").change(function(event) {
+	var isSelected = $(this).find("option:selected").length;
+	if (isSelected) {
+		$("#del-combo").attr('disabled', false);
+	}
+});
+
+$("#del-combo").click(function(event) {
+	$("#combos").find("option:selected").remove();
+});
+
+$(document).on('click', '#new-combo>.btn', function(event) {
+	var fp = $("#new-combo-fp");
+	var tp = $("#new-combo-tp");
+
+	if(fp.val().length > 0 && tp.val().length > 0) {
+		$("#combos").append("<option>\"" + fp.val() + "\" - \"" + tp.val() + "\"</option>");
+		fp.val("");
+		tp.val("");
+	}
+});
